378c0149db02db9d7211243e30b3dc729a11e9bd
Johann Garces <JGarces@national.aaa.com>
2016-02-03 09:33:41 -0500

    Initial Commit

---
+++ ColorBytesToFloat/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ColorBytesToFloat
+//
+//  Created by Garces, Johann (TEKsystems) on 2/3/16.
+//  Copyright © 2016 johannmg. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
