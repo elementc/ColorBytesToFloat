bc0bfab2cdf405b0d0d163e4cfc5520585c560b5
Johann Garces <JGarces@national.aaa.com>
2016-02-03 16:51:16 -0500

    first usable tool made. used it this morning, should improve it b/c I'm sure to use it later.

--- ColorBytesToFloat/main.swift
+++ ColorBytesToFloat/main.swift
@@ -1,4 +1,3 @@
-//
 //  main.swift
 //  ColorBytesToFloat
 //
@@ -9,4 +8,53 @@
 import Foundation
 
 print("Hello, World!")
+
+extension String {
+    var floatVal: Float {
+        return (self as NSString).floatValue
+    }
+}
+
+extension Float {
+    var fff: String {
+        return String(format: "%.3f", self)
+    }
+}
+
+
+
+var response: String = ""
+var kb = NSFileHandle.fileHandleWithStandardInput()
+var inputData: NSData?
+print ("q to quit")
+print ("'trio' to set to triple RBG mode")
+
+func runHexMode(){
+    print ("running hex mode: input should be  <RRR,GGG,BBB> ")
+    while response != "q" {
+        inputData = kb.availableData
+        let tempdirty = NSString(data: inputData!, encoding: NSUTF8StringEncoding) as! String
+        response = tempdirty.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        
+        let varArray:[String] = response.componentsSeparatedByCharactersInSet(NSCharacterSet(charactersInString: " ,./"))
+        print("UIColor(red: \((varArray[0].floatVal/255.0).fff), green: \((varArray[1].floatVal/255.0).fff), blue: \((varArray[2].floatVal/255.0).fff), alpha: 1.00)")
+    }
+    
+    
+}
+
+while response != "q" {
+    inputData = kb.availableData
+    let tempdirty = NSString(data: inputData!, encoding: NSUTF8StringEncoding) as! String
+    response = tempdirty.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+    
+    if (response == "trio"){
+        runHexMode()
+        break //i know this is a weird control flow, i'm mkaing this super dirty/fast
+    }
+    else {
+        print ("\(response) => \(response.floatVal / 255.0)" )
+    }
+    
+}
 
